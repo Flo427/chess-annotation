a3dc798b6d364fae4c2dbf289307548f7457cf13
Florian Beck <florian.beck.4@gmx.de>
2018-09-20 01:17:37 +0200

    top words list fixed, class mapping added and old print statements deleted

--- classification.py
+++ classification.py
@@ -11,7 +11,7 @@ files = [
 	'chessdoctor.pgn',
 	'd00_chess_informant.pgn',
 	'electronic_campfire.pgn',
-	'europe_echecs.pgn',
+#	'europe_echecs.pgn',
 	'exeter_lessons_from_tal.pgn',
 	'famous_games.pgn',
 	'GM_games.pgn',
@@ -20,7 +20,7 @@ files = [
 	'hayes.pgn',
 	'human_computer.pgn',
 	'immortal_games.pgn',
-	'kasp_top.pgn',
+#	'kasp_top.pgn',
 	'kk.pgn',
 	'koltanowski.pgn',
 	'kramnik.pgn',
@@ -29,7 +29,7 @@ files = [
 	'middleg.pgn',
 	'moscow64.pgn',
 	'newyork1924.pgn',
-	'perle.pgn',
+#	'perle.pgn',
 	'polgar.pgn',
 	'pon_korch.pgn',
 	'romero.pgn',
@@ -46,45 +46,76 @@ files = [
 	'wijk_2004_annotated.pgn',
 	'world_matches.pgn',
 ]
+
+dict = {
+	"1": 1,
+	"2": 2,
+	"3": 3,
+	"4": 4,
+	"5": 5,
+	"6": 6,
+	"!": 1,
+	"?": 2,
+	"!!": 3,
+	"??": 4,
+	"!?": 5,
+	"?!": 6,
+	"7": 0,
+	"8": 0,
+	"10": 0,
+	"11": 11,
+	"12": 11,
+	"13": 13,
+	"14": 14,
+	"15": 15,
+	"16": 14,
+	"17": 15,
+	"18": 14,
+	"19": 15,
+	"22": 0,
+	"32": 0,
+	"36": 0,
+	"40": 0,
+	"44": 0,
+	"132": 0,
+	"133": 0,
+	"138": 0,
+	"142": 0,
+	"146": 0,
+}
+
 comment_data = []
 
 def read_comments_of_file(file, comment_data, cfd):
 	raw = open(file, 'rb').read()
 	str = raw.decode('iso-8859-1')
 
-	comments_with_nag = re.findall(r'\$(?P<class>[0-9]+)\s*{(?P<comment>[^{}]*)}', str)
-	print(file, len(comments_with_nag))
-
-	comment_data += [(pair[1].lower().split(), pair[0])
-		for pair in comments_with_nag]
+	comments = re.findall(r'\$(?P<class>[0-9]+)\s*{(?P<comment>[^{}]*)}', str)		#NAG
+	comments += re.findall(r'(?P<class>[!\?]{1,2})\s*{(?P<comment>[^{}]*)}', str)	#symbol
 		
-	comments_with_symbol = re.findall(r'(?P<class>[!\?]{1,2})\s*{(?P<comment>[^{}]*)}', str)
-	print(file, len(comments_with_symbol))
-		
-	comment_data += [(pair[1].lower().split(), pair[0])
-		for pair in comments_with_symbol]
+	comment_data += [(pair[1].lower().split(), dict[pair[0]]) for pair in comments if dict[pair[0]]]
 
 	fdist = nltk.FreqDist()
-	for pair in comments_with_symbol:
-		fdist[pair[0]] += 1
-	#print(fdist.most_common(1000))
+	for pair in comments:
+		fdist[dict[pair[0]]] += 1
 	cfd[file] = fdist
 
 cfd = nltk.ConditionalFreqDist()	
 for file in files:
-	read_comments_of_file('files/' + file, comment_data, cfd)
-	
-cfd.tabulate()
+	read_comments_of_file('files/' + file, comment_data, cfd)	
+#cfd.tabulate()
 	
 random.shuffle(comment_data)
-#print(comment_data[:5])
 
 """
 Ectract classification features
 """
 
 threshold = 1000
-fd_all_words = nltk.FreqDist((w for w in words) for (words,_) in comment_data)
+fd_all_words = nltk.FreqDist()
+for (words,_) in comment_data:
+	for w in words:
+		fd_all_words[w] += 1
 top_words = [word for (word, freq) in fd_all_words.most_common(threshold)]
 
 def rating_features(comment):
@@ -98,7 +129,6 @@ Prepare data
 """
 
 count = len(comment_data)
-#print(count)
 
 cut1 = int(0.7 * count)
 cut2 = int(0.9 * count)
@@ -106,7 +136,6 @@ cut2 = int(0.9 * count)
 featuresets = [(rating_features(c), g) for (c,g) in comment_data]
 training_data, devtest_data, test_data = comment_data[:cut1], comment_data[cut1:cut2], comment_data[cut2:]
 training_fdata, devtest_fdata, test_fdata = featuresets[:cut1], featuresets[cut1:cut2], featuresets[cut2:]
-#print(training_data[:5])
 
 """
 Train a classifier
@@ -132,6 +161,6 @@ for (comment, tag) in test_data:
 #	print('correct=%-8s guess=%-8s comment=%-100s' % (tag, guess, comment))
 	
 print(nltk.classify.accuracy(classifier, test_fdata))
-#print(classifier.show_most_informative_features(5))
+print(classifier.show_most_informative_features(5))
 
 print(nltk.ConfusionMatrix(tags,guesses).pretty_format())

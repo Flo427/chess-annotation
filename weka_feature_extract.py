df265cd3473358e3faa351f7d865585f6ee46cbf
Flo427 <florian.beck.4@gmx.de>
2018-11-14 01:06:09 +0100

    arff-file created to use Weka for classifying

---
+++ weka_feature_extract.py
@@ -0,0 +1,147 @@
+import nltk
+import random
+import re
+
+"""
+Extract comments of .pgn-file
+"""
+
+files = [
+	'bali02.pgn',
+	'chessdoctor.pgn',
+#	'd00_chess_informant.pgn',
+#	'electronic_campfire.pgn',
+#	'europe_echecs.pgn',
+#	'exeter_lessons_from_tal.pgn',
+#	'famous_games.pgn',
+#	'GM_games.pgn',
+#	'great_masters.pgn',	
+#	'hartwig.pgn',
+#	'hayes.pgn',
+#	'human_computer.pgn',
+#	'immortal_games.pgn',
+#	'kasp_top.pgn',
+#	'kk.pgn',
+#	'koltanowski.pgn',
+	'kramnik.pgn',
+#	'linares_2001.pgn',
+	'linares_2002.pgn',
+#	'middleg.pgn',
+#	'moscow64.pgn',
+#	'newyork1924.pgn',
+#	'perle.pgn',
+	'polgar.pgn',
+#	'pon_korch.pgn',
+#	'romero.pgn',
+#	'russian_chess.pgn',
+#	'scarborough_2001.pgn',
+	'scca.pgn',
+#	'schiller.pgn',
+#	'semicomm.pgn',
+	'top_games.pgn',
+#	'vc_89_99.pgn',
+#	'vc_2000.pgn',
+#	'vc_2001.pgn',
+#	'wijk_2003_annotated.pgn',
+#	'wijk_2004_annotated.pgn',
+#	'world_matches.pgn',
+]
+
+dict = {
+	"1": 1,
+	"2": 2,
+	"3": 1,
+	"4": 2,
+	"5": 1,
+	"6": 2,
+	"!": 1,
+	"?": 2,
+	"!!": 1,
+	"??": 2,
+	"!?": 1,
+	"?!": 2,
+	"7": 0,
+	"8": 0,
+	"10": 0,
+	"11": 0,
+	"12": 0,
+	"13": 0,
+	"14": 0,
+	"15": 0,
+	"16": 0,
+	"17": 0,
+	"18": 0,
+	"19": 0,
+	"22": 0,
+	"32": 0,
+	"36": 0,
+	"40": 0,
+	"44": 0,
+	"132": 0,
+	"133": 0,
+	"138": 0,
+	"142": 0,
+	"146": 0,
+}
+
+comment_data = []
+
+def read_comments_of_file(file, comment_data, cfd):
+	raw = open(file, 'rb').read()
+	str = raw.decode('iso-8859-1')
+
+	comments = re.findall(r'\$(?P<class>[0-9]+)\s*{(?P<comment>[^{}]*)}', str)		#NAG
+	comments += re.findall(r'(?P<class>[!\?]{1,2})\s*{(?P<comment>[^{}]*)}', str)	#symbol
+		
+	comment_data += [(pair[1].lower().split(), dict[pair[0]]) for pair in comments if dict[pair[0]]]
+
+	fdist = nltk.FreqDist()
+	for pair in comments:
+		fdist[dict[pair[0]]] += 1
+	cfd[file] = fdist
+
+cfd = nltk.ConditionalFreqDist()	
+for file in files:
+	read_comments_of_file('files/' + file, comment_data, cfd)	
+#cfd.tabulate()
+	
+random.shuffle(comment_data)
+
+"""
+Ectract classification features
+"""
+
+from nltk.corpus import stopwords
+
+threshold = 100
+fd_all_words = nltk.FreqDist()
+for (words,_) in comment_data:
+	for w in set(words) - set(stopwords.words('english')):
+		fd_all_words[w] += 1
+top_words = [word for (word, freq) in fd_all_words.most_common(threshold)]
+
+def rating_features(comment):
+	features = {}
+	for word in top_words:
+		features[word] = comment.count(word)
+	return features
+
+featuresets = [(rating_features(c), g) for (c,g) in comment_data]
+
+def write_arff():
+    arff = open("chess-annotations.arff", "w")
+    RELATION_NAME = "comment"									 
+    arff.write("@RELATION " + RELATION_NAME + "\n")
+    for word in top_words:
+        arff.write("@ATTRIBUTE COUNT(" + str(word).replace("'", "").replace(",", "") + ") REAL\n")
+    arff.write("@ATTRIBUTE POSITIVE {True, False}\n") 
+    arff.write("@DATA\n")
+    for (features, g) in featuresets:
+        buff = ""
+        if 2 - g:
+            buff += (",".join([str(features[f]) for f in features]) + ', True' + "\n")
+        else:
+            buff += (",".join([str(features[f]) for f in features]) + ', False' + "\n")
+        arff.write(buff)
+
+write_arff()  

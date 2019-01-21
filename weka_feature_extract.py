0c5ba33a4f30dcc2ea29f896747791e07e1e8a6a
Flo427 <florian.beck.4@gmx.de>
2019-01-21 11:35:27 +0100

    Stemming, Bigrams and trigrams

--- weka_feature_extract.py
+++ weka_feature_extract.py
@@ -85,11 +85,11 @@ from nltk.tokenize import RegexpTokenizer
 def read_comments_of_file(file, comment_data):
 	raw = open(file, 'rb').read()
 	str = raw.decode('iso-8859-1')
-	tokenizer = RegexpTokenizer('(?:\w\.)+|[\w\d\-\']+|\.+|#[\w\d]{2}|1/2|[!\?]+|[\-\+/=]+|\S|\s+')
+	tokenizer = RegexpTokenizer('(?:\w\.)+|[\w\d\-\']+|\.+|#[\w\d]{2}|1/2|[!\?]+|[\-\+/=]+|\S')
     
 	comments = re.findall(r'\$(?P<class>[0-9]+)\s*{(?P<comment>[^{}]*)}', str)		#NAG
 	comments += re.findall(r'(?P<class>[!\?]{1,2})\s*{(?P<comment>[^{}]*)}', str)	#symbol
-		
+
 	comment_data += [(tokenizer.tokenize(pair[1].lower()), dict[pair[0]]) for pair in comments if dict[pair[0]]]
 
 comment_data = []
@@ -101,20 +101,39 @@ Ectract classification features
 """
 
 from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
 
 def rating_features(comment):
 	features = {}
 	for word in top_words:
 		features[word] = comment.count(word)
+	for bigram in top_bigrams:
+		features[bigram] = comment.count(bigram)
+	for trigram in top_trigrams:
+		features[trigram] = comment.count(trigram)
 	return features
 
-threshold = 1000
+#threshold = 1000
 fd_all_words = nltk.FreqDist()
+fd_all_bigrams = nltk.FreqDist()
+fd_all_trigrams = nltk.FreqDist()
+stemmer = PorterStemmer()
 for (words,_) in comment_data:
-	for w in set(words):
+	#print(words)
+	#words = [stemmer.stem(word) for word in words]
+	for w in words: #set(words) - set(stopwords.words('english')):
 		fd_all_words[w] += 1
-print(len(fd_all_words))
-top_words = [word for (word, freq) in fd_all_words.most_common()]
+	if len(words) > 1:
+		for b in nltk.bigrams(words):
+			fd_all_bigrams[b] += 1
+		for t in nltk.trigrams(words):
+			fd_all_trigrams[t] += 1
+top_words = [word for (word, freq) in fd_all_words.most_common() if freq > 25]
+top_bigrams = [bigram for (bigram, freq) in fd_all_bigrams.most_common() if freq > 500]
+top_trigrams = [trigram for (trigram, freq) in fd_all_trigrams.most_common() if freq > 50]
+print(len(top_words))
+print(len(top_bigrams))
+print(len(top_trigrams))
 featuresets = [(rating_features(c), g) for (c,g) in comment_data]
 
 """
@@ -126,7 +145,11 @@ def write_arff():
     RELATION_NAME = "comment"									 
     arff.write("@RELATION " + RELATION_NAME + "\n")
     for word in top_words:
-        arff.write("@ATTRIBUTE COUNT(" + str(word).replace(" ", "_blank_").replace("\n", "_new_line_").replace("\t", "_tab_").replace("\r", "_carriage_return_").replace("\"", "_quote_").replace("'", "_apostrophe_").replace(",", "_comma_").replace("%", "_percent_") + ") INTEGER\n")
+        arff.write("@ATTRIBUTE COUNT(" + str(word).replace("\"", "_quote_").replace("'", "_apostrophe_").replace(",", "_comma_").replace("%", "_percent_") + ") INTEGER\n")
+    for bigram in top_bigrams:
+        arff.write("@ATTRIBUTE COUNT(" + str(bigram).replace("\"", "_quote_").replace(" ", "").replace("'", "").replace(",", "_").replace("%", "_percent_") + ") INTEGER\n")
+    for trigram in top_trigrams:
+        arff.write("@ATTRIBUTE COUNT(" + str(trigram).replace("\"", "_quote_").replace(" ", "").replace("'", "").replace(",", "_").replace("%", "_percent_") + ") INTEGER\n")
     arff.write("@ATTRIBUTE CLASS {1, 2, 3, 4, 5, 6}\n") 
     arff.write("@DATA\n")
     for (features, g) in featuresets:

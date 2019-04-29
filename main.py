6d272def5363994405d41bcba9b990f311189359
Flo427 <florian.beck.4@gmx.de>
2019-04-29 04:06:32 +0200

    simplified count and tfidf extraction

--- main.py
+++ main.py
@@ -7,11 +7,9 @@ MIN_COMMENT_LENGTH = 3
 MAX_COMMENT_LENGTH = 49
 MIN_ENGLISH_WORDS = 3
 LOWERCASE = True
-STEMMING = False
-REMOVE_STOPWORDS = False
-MIN_FREQ_WORD = 1
-MIN_FREQ_BIGRAM = 1
-MIN_FREQ_TRIGRAM = 1
+LEMMATIZING = False
+MIN_FREQ = 1
+MAX_FEATURES = 100000
 
 files = [
 	'bali02.pgn',
@@ -270,11 +268,8 @@ output_class_position_2 = {
 	"146": 0,
 }
 
-from collections import Counter
-from nltk.corpus import stopwords
 from nltk.tokenize import RegexpTokenizer
 from nltk.stem.wordnet import WordNetLemmatizer
-from nltk.stem.porter import PorterStemmer
 from langid.langid import LanguageIdentifier, model
 
 start = time.time()
@@ -282,7 +277,6 @@ random.seed(0)
 
 tokenizer = RegexpTokenizer('#[\w\d]{2}|\$\d+|[!\?]+|[\-\+/=]+|1/2|(?:\w\.)+|\.+|[\w\d\-\']+|\S')
 lemmatizer = WordNetLemmatizer()
-stemmer = PorterStemmer()
 identifier = LanguageIdentifier.from_modelstring(model, norm_probs=True)
 english_vocab = set(w.lower() for w in nltk.corpus.words.words())
 
@@ -292,7 +286,7 @@ Extract comments of .pgn-file
 
 print("\nEXTRACT COMMENTS OF PGN-FILE\n")
 
-def read_comments_of_file(file, comment_data_total, comment_data_move, comment_data_position):
+def read_comments_of_file(file, comment_data):
 	raw = open(file, 'rb').read()
 	str = raw.decode('iso-8859-1')
 	
@@ -309,6 +303,8 @@ def read_comments_of_file(file, comment_data_total, comment_data_move, comment_d
 			print(instances, instances_total, end - start)
 		if LOWERCASE:
 			comment = tokenizer.tokenize(pair[1].lower())
+			if LEMMATIZING:
+				comment = [lemmatizer.lemmatize(lemmatizer.lemmatize(token, 'n'), 'v') for token in comment]
 		else:
 			comment = tokenizer.tokenize(pair[1])
 		if len(comment) < MIN_COMMENT_LENGTH or len(comment) > MAX_COMMENT_LENGTH:
@@ -325,19 +321,13 @@ def read_comments_of_file(file, comment_data_total, comment_data_move, comment_d
 					count += 1
 			if count == MIN_ENGLISH_WORDS:
 				instances += 1
-				comment_data_total += [(comment, pair[0])]
-				if output_class_move_1[pair[0]]:
-					comment_data_move += [(comment, pair[0])]
-				if output_class_position_1[pair[0]]:
-					comment_data_position += [(comment, pair[0])]
+				comment_data += [(comment, pair[0])]
 					
-comment_data_total = []
-comment_data_move = []
-comment_data_position = []
+comment_data = []
 instances = 0
 instances_total = 0
 for file in files:
-	read_comments_of_file('files/' + file, comment_data_total, comment_data_move, comment_data_position)
+	read_comments_of_file('files/' + file, comment_data)
 end = time.time()
 print(instances, instances_total, end - start)
 
@@ -347,89 +337,16 @@ Ectract classification features
 
 print("\nEXTRACT CLASSIFICATION FEATURES\n")
 
-def tf_features(comment, top_words, top_bigrams, top_trigrams):
-	features = Counter()
-	features['NUMBER_OF_TOKENS'] = len(comment)
-	for word in comment:
-		if word in top_words:
-			features[word] += 1
-	for bigram in nltk.bigrams(comment):
-		if bigram in top_bigrams:
-			features[bigram] += 1
-	for trigram in nltk.trigrams(comment):
-		if trigram in top_trigrams:
-			features[trigram] += 1
-	return features
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from scipy.sparse import coo_matrix
 
-def create_tf_featuresets(comment_data, top_words_total, top_bigrams_total, top_trigrams_total, top_words_move, top_bigrams_move, top_trigrams_move, top_words_position, top_bigrams_position, top_trigrams_position):
-	fd_words_total = nltk.FreqDist()
-	fd_bigrams_total = nltk.FreqDist()
-	fd_trigrams_total = nltk.FreqDist()
-	fd_words_move = nltk.FreqDist()
-	fd_bigrams_move = nltk.FreqDist()
-	fd_trigrams_move = nltk.FreqDist()
-	fd_words_position = nltk.FreqDist()
-	fd_bigrams_position = nltk.FreqDist()
-	fd_trigrams_position = nltk.FreqDist()
-	
-	global instances
-	for (words, label) in comment_data:
-		instances += 1
-		if not (instances % 1000):
-			end = time.time()
-			print(instances, end - start)
-		if STEMMING:
-			words = [stemmer.stem(word) for word in words]
-		if REMOVE_STOPWORDS:
-			words = set(words) - set(stopwords.words('english'))
-		for word in words:
-			fd_words_total[word] += 1
-			if output_class_move_1[label]:
-				fd_words_move[word] += 1
-			if output_class_position_1[label]:
-				fd_words_position[word] += 1
-		if len(words) > 1:
-			for bigram in nltk.bigrams(words):
-				fd_bigrams_total[bigram] += 1
-				if output_class_move_1[label]:
-					fd_bigrams_move[bigram] += 1
-				if output_class_position_1[label]:
-					fd_bigrams_position[bigram] += 1
-			for trigram in nltk.trigrams(words):
-				fd_trigrams_total[trigram] += 1
-				if output_class_move_1[label]:
-					fd_trigrams_move[trigram] += 1
-				if output_class_position_1[label]:
-					fd_trigrams_position[trigram] += 1
-	top_words_total += [word for (word, freq) in fd_words_total.most_common() if freq > MIN_FREQ_WORD]
-	top_bigrams_total += [bigram for (bigram, freq) in fd_bigrams_total.most_common() if freq > MIN_FREQ_BIGRAM]
-	top_trigrams_total += [trigram for (trigram, freq) in fd_trigrams_total.most_common() if freq > MIN_FREQ_TRIGRAM]
-	top_words_move += [word for (word, freq) in fd_words_move.most_common() if freq > MIN_FREQ_WORD]
-	top_bigrams_move += [bigram for (bigram, freq) in fd_bigrams_move.most_common() if freq > MIN_FREQ_BIGRAM]
-	top_trigrams_move += [trigram for (trigram, freq) in fd_trigrams_move.most_common() if freq > MIN_FREQ_TRIGRAM]
-	top_words_position += [word for (word, freq) in fd_words_position.most_common() if freq > MIN_FREQ_WORD]
-	top_bigrams_position += [bigram for (bigram, freq) in fd_bigrams_position.most_common() if freq > MIN_FREQ_BIGRAM]
-	top_trigrams_position += [trigram for (trigram, freq) in fd_trigrams_position.most_common() if freq > MIN_FREQ_TRIGRAM]
-	print("Top words/bigrams/trigrams total:")
-	print(len(top_words_total), len(top_bigrams_total), len(top_trigrams_total))
-	print("Top words/bigrams/trigrams move:")
-	print(len(top_words_move), len(top_bigrams_move), len(top_trigrams_move))
-	print("Top words/bigrams/trigrams position:")
-	print(len(top_words_position), len(top_bigrams_position), len(top_trigrams_position))
-	featuresets = [(tf_features(comment, top_words_total, top_bigrams_total, top_trigrams_total), label) for (comment, label) in comment_data]
-	return featuresets
-	
-top_words_total = []
-top_bigrams_total = []
-top_trigrams_total = []
-top_words_move = []
-top_bigrams_move = []
-top_trigrams_move = []
-top_words_position = []
-top_bigrams_position = []
-top_trigrams_position = []
-instances = 0
-featuresets = create_tf_featuresets(comment_data_total, top_words_total, top_bigrams_total, top_trigrams_total, top_words_move, top_bigrams_move, top_trigrams_move, top_words_position, top_bigrams_position, top_trigrams_position)
+comments = [" ".join(token) for (token,_) in comment_data]
+count_vectorizer = CountVectorizer(min_df=MIN_FREQ, max_df=1.0, max_features=MAX_FEATURES, ngram_range=(1,3))
+count_matrix = count_vectorizer.fit_transform(comments)
+count_feature_names = count_vectorizer.get_feature_names()
+tfidf_vectorizer = TfidfVectorizer(min_df=MIN_FREQ, max_df=1.0, max_features=MAX_FEATURES, ngram_range=(1,3))
+tfidf_matrix = tfidf_vectorizer.fit_transform(comments)
+tfidf_feature_names = tfidf_vectorizer.get_feature_names()
 
 end = time.time()
 print(end - start)
@@ -440,41 +357,32 @@ Write arff files
 
 print("\nWRITE ARFF FILES\n")
 
-def tf_write_arff(problem_name, classes, output_class, top_words, top_bigrams, top_trigrams):
-    arff = open("chess-annotations-tf-" + problem_name + ".arff", "w")
+def write_arff(feature_type, problem_name, classes, output_class, feature_names, matrix):
+    arff = open("chess-annotations-" + feature_type + "-" + problem_name + ".arff", "w")
     RELATION_NAME = "comment-" + problem_name									 
     buff = ("@RELATION " + RELATION_NAME + "\n")
-    buff += ("@ATTRIBUTE NUMBER_OF_TOKENS INTEGER\n") 
-    for word in top_words:
-        buff += ("@ATTRIBUTE COUNT(" + str(word).replace("\"", "_quote_").replace("'", "_apostrophe_").replace(",", "_comma_").replace("%", "_percent_") + ") INTEGER\n")
-    for bigram in top_bigrams:
-        buff += ("@ATTRIBUTE COUNT(" + str(bigram[0]).replace("\"", "_quote_").replace("'", "_apostrophe_").replace(",", "_comma_").replace("%", "_percent_")
-		+ "_" + str(bigram[1]).replace("\"", "_quote_").replace("'", "_apostrophe_").replace(",", "_comma_").replace("%", "_percent_") + ") INTEGER\n")
-    for trigram in top_trigrams:
-        buff += ("@ATTRIBUTE COUNT(" + str(trigram[0]).replace("\"", "_quote_").replace("'", "_apostrophe_").replace(",", "_comma_").replace("%", "_percent_")
-		 + "_" + str(trigram[1]).replace("\"", "_quote_").replace("'", "_apostrophe_").replace(",", "_comma_").replace("%", "_percent_")
-		  + "_" + str(trigram[2]).replace("\"", "_quote_").replace("'", "_apostrophe_").replace(",", "_comma_").replace("%", "_percent_") + ") INTEGER\n")
-    buff += ("@ATTRIBUTE CLASS {" + classes + "}\n") 
-    buff += ("@DATA\n")
-    for (features, label) in featuresets:
-        buff += ("{" + str(len(top_words) + len(top_bigrams) + len(top_trigrams)) + " " + str(features['NUMBER_OF_TOKENS']) + ", ")        
-        for idx, f in enumerate(top_words):
-            if features[f] > 0:
-                buff += (str(idx) + " " + str(features[f]) + ", ")
-        for idx, f in enumerate(top_bigrams):
-            if features[f] > 0:
-                buff += (str(len(top_words) + idx) + " " + str(features[f]) + ", ")
-        for idx, f in enumerate(top_trigrams):
-            if features[f] > 0:
-                buff += (str(len(top_words) + len(top_bigrams) + idx) + " " + str(features[f]) + ", ")
-        buff += (str(len(top_words) + len(top_bigrams) + len(top_trigrams) + 1) + " " + str(output_class[label]) + "}\n")        
+    buff += ("@ATTRIBUTE number_of_tokens INTEGER\n") 
+    for feature in feature_names:
+        buff += ("@ATTRIBUTE " + feature_type + "(" + str(feature).replace("\"", "_quote_").replace("'", "_apostrophe_").replace(",", "_comma_").replace("%", "_percent_") + ") REAL\n")
+    buff += ("@ATTRIBUTE class {" + classes + "}\n") 
+    buff += ("@DATA\n")    
+    for comment in range(len(comment_data)):
+        if not output_class[comment_data[comment][1]]:
+            continue
+        row = coo_matrix(matrix[comment,:])
+        row = sorted(zip(row.col, row.data), key=lambda x: (x[0]))
+        buff += ("{0 " + str(len(comment_data[comment][0])) + ", ")
+        for feature, value in row:            
+            buff += (str(feature + 1) + " " + str(value) + ", ")
+        buff += (str(len(feature_names) + 1) + " " + str(output_class[comment_data[comment][1]]) + "}\n")        
     arff.write(buff)
 
-tf_write_arff("total", "1, 2", output_class_total, top_words_total, top_bigrams_total, top_trigrams_total)  
-tf_write_arff("move-1", "1, 2", output_class_move_1, top_words_move, top_bigrams_move, top_trigrams_move)  
-tf_write_arff("move-2", "1, 2, 3, 4, 5, 6", output_class_move_2, top_words_move, top_bigrams_move, top_trigrams_move)  
-tf_write_arff("position-1", "1, 2, 3", output_class_position_1, top_words_position, top_bigrams_position, top_trigrams_position)  
-tf_write_arff("position-2", "1, 2, 3, 4, 5, 6, 7", output_class_position_2, top_words_position, top_bigrams_position, top_trigrams_position)  
+for (feature_type, feature_names, matrix) in [("count", count_feature_names, count_matrix), ("tfidf", tfidf_feature_names, tfidf_matrix)]:
+	write_arff(feature_type, "total", "1, 2", output_class_total, feature_names, matrix) 
+	write_arff(feature_type, "move-1", "1, 2", output_class_move_1, feature_names, matrix)  
+	write_arff(feature_type, "move-2", "1, 2, 3, 4, 5, 6", output_class_move_2, feature_names, matrix)  
+	write_arff(feature_type, "position-1", "1, 2, 3", output_class_position_1, feature_names, matrix)  
+	write_arff(feature_type, "position-2", "1, 2, 3, 4, 5, 6, 7", output_class_position_2, feature_names, matrix)  
 
 end = time.time()
 print(end - start)
